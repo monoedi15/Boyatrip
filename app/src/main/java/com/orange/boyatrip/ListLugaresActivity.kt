f8f2df952884ba4cee1238f01971445326160fac
Jesus Rdoriguez <JesusRodAltamar@gmail.com>
2021-11-21 15:48:50 -0500

    CONSTRUCION DEL ADAPTER

--- app/src/main/java/com/orange/boyatrip/ListLugaresActivity.kt
+++ app/src/main/java/com/orange/boyatrip/ListLugaresActivity.kt
@@ -2,10 +2,20 @@ package com.orange.boyatrip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class ListLugaresActivity : AppCompatActivity() {
+
+    private lateinit var listaLugares: ArrayList<SitioInteres>
+    private lateinit var lugaresInteresAdapter: LugaresInteresAdapter
+    private lateinit var lugaresInteresRecyclerView: RecyclerView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_lugares)
+
+        lugaresInteresRecyclerView = findViewById(R.id.lugares_recycler_view)
     }
 }
---
+++ app/src/main/java/com/orange/boyatrip/LugaresInteresAdapter.kt
@@ -0,0 +1,39 @@
+package com.orange.boyatrip
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class LugaresInteresAdapter (private val lugarisinteresList: ArrayList<SitioInteres>) : RecyclerView.Adapter<LugaresInteresAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.activity_list_lugares,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val sitiosInteres = lugarisinteresList[position]
+        holder.bind(sitiosInteres)
+    }
+
+    override fun getItemCount(): Int {
+        return lugarisinteresList.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        private var tituloPOTextView : TextView = itemView.findViewById<>(R.id.tituloPO_text_view)
+        private var puntuacionPOTextview : TextView = itemView.findViewById<>(R.id.puntuacionPO_text_view)
+        private var descripcionPOTextview : TextView = itemView.findViewById<>(R.id.descripcionPO_text_view)
+        private var pictureImageView: ImageView = itemView.findViewById<>(R.id.picture_image_view)
+        fun bind(sitiosInteres:SitioInteres){
+            tituloPOTextView.text = sitiosInteres.nombre
+            puntuacionPOTextview = sitiosInteres.puntuacion
+            descripcionPOTextview.text = sitiosInteres.descripcion
+            //picture
+        }
+
+    }
+}
