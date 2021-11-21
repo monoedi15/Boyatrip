524755afb11b9a296ec1af43069b874b1f5abf8b
Jesus Rdoriguez <JesusRodAltamar@gmail.com>
2021-11-21 12:47:24 -0500

    SE CREA BASE PARA CARDVIEW

---
+++ app/src/main/java/com/orange/boyatrip/ListLugaresActivity.kt
@@ -0,0 +1,11 @@
+package com.orange.boyatrip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListLugaresActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_lugares)
+    }
+}
